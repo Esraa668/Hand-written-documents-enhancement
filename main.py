4fd5afe67fc9aebef92625907a6b4cde00ab08f7
Esraa nashaat <esraanashaat07@gmail.com>
2025-05-12 14:13:44 +0300

    grayscale processing and file validation

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import numpy as np
 import os
 from matplotlib import pyplot as plt
 
+def enhance_document(image_path, output_dir="output"):
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    original = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE) #grayscale conversion
+    if original is None:
+        raise ValueError(f"Image not found at {image_path}") #checks if image exists in the destined path
